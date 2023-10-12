f3fc9843966983261b4ebd23cd4c55a964b25bb9
Justin Smith <justin.smith@frgrisk.com>
2023-10-12 10:53:02 -0400

    Format code

--- 2022/rust/day4/part2/src/main.rs
+++ 2022/rust/day4/part2/src/main.rs
@@ -5,20 +5,21 @@ fn main() {
     let file_path: &str = parse_configs(&args);
     let mut total_overlap = 0;
     for line in fs::read_to_string(file_path).unwrap().lines() {
-        let elf_1_assignment = get_assignment(line,1);
-        let elf_2_assignment = get_assignment(line,2);
+        let elf_1_assignment = get_assignment(line, 1);
+        let elf_2_assignment = get_assignment(line, 2);
         println!("\n");
-        println!("{:?}",line);
-        println!("{}",total_overlap);
-        if get_lower_assignment(elf_1_assignment) >= get_lower_assignment(elf_2_assignment) &&
-            get_lower_assignment(elf_1_assignment) <= get_higher_assignment(elf_2_assignment) {
-            total_overlap = total_overlap+1;
-        } else
-        if get_lower_assignment(elf_2_assignment) >= get_lower_assignment(elf_1_assignment) &&
-            get_lower_assignment(elf_2_assignment) <= get_higher_assignment(elf_1_assignment) {
-            total_overlap = total_overlap+1;
+        println!("{:?}", line);
+        println!("{}", total_overlap);
+        if get_lower_assignment(elf_1_assignment) >= get_lower_assignment(elf_2_assignment)
+            && get_lower_assignment(elf_1_assignment) <= get_higher_assignment(elf_2_assignment)
+        {
+            total_overlap = total_overlap + 1;
+        } else if get_lower_assignment(elf_2_assignment) >= get_lower_assignment(elf_1_assignment)
+            && get_lower_assignment(elf_2_assignment) <= get_higher_assignment(elf_1_assignment)
+        {
+            total_overlap = total_overlap + 1;
         }
-        println!("{}",total_overlap);
+        println!("{}", total_overlap);
     }
     println!("Hello, world!");
 }
@@ -29,17 +30,15 @@ fn parse_configs(args: &[String]) -> &str {
 }
 
 fn get_assignment(text: &str, i: usize) -> Option<&str> {
-    text.split(',')
-        .skip(i-1)
-        .next()
+    text.split(',').skip(i - 1).next()
 }
 
 fn get_lower_assignment(text: Option<&str>) -> i32 {
-    let v: Vec<&str> = text.unwrap().split(|c| c=='-').collect();
+    let v: Vec<&str> = text.unwrap().split(|c| c == '-').collect();
     return v[0].parse().unwrap();
 }
 
 fn get_higher_assignment(text: Option<&str>) -> i32 {
-    let v: Vec<&str> = text.unwrap().split(|c| c=='-').collect();
+    let v: Vec<&str> = text.unwrap().split(|c| c == '-').collect();
     return v[1].parse().unwrap();
 }
